8f1eeb751c94f6d584cffaf2d2851c972d06383b
summerijo <152608016+summerijo@users.noreply.github.com>
2024-05-03 14:38:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ def upload_file():
         
         return render_template('index.html', result_image=result_image)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
